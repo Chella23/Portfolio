609e3b3cdf59893c0edff03c02f79c9e55b0b52e
Chella <152418665+Chella23@users.noreply.github.com>
2025-07-10 12:55:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: '/Portfolio/',
+  
   server: {
     host: "::",
     port: 8080,
   },
+  base: '/Portfolio/',
   plugins: [
     react(),
     mode === 'development' &&
