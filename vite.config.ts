f6b556549fff4e86a6d2234a9502808f8a738204
Chella <152418665+Chella23@users.noreply.github.com>
2025-07-10 13:30:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: './',
+  base: '/Portfolio/',
   plugins: [
     react(),
     mode === 'development' &&
